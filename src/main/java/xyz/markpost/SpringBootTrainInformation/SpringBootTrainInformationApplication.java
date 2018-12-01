9a642da61d5b1220290c1f894e44cb0204595e2c
Mark Post <mark.post@live.nl>
2018-12-01 15:02:28 +0100

    Initial project structure

---
+++ src/main/java/xyz/markpost/SpringBootTrainInformation/SpringBootTrainInformationApplication.java
@@ -0,0 +1,12 @@
+package xyz.markpost.SpringBootTrainInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainInformationApplication.class, args);
+	}
+}
---
+++ src/test/java/xyz/markpost/SpringBootTrainInformation/SpringBootTrainInformationApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.markpost.SpringBootTrainInformation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTrainInformationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
